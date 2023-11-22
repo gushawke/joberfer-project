9d91ced12cbec8c7a2d997e0646b38fc17b9cc96
gus.hawke <gus.hawke@gmail.com>
2023-11-22 14:52:48 -0700

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Joberfer Project</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link href="{{ asset('build/assets/app-8ccbb554.css') }}" rel="stylesheet" />
 
         <!-- Styles -->
         <style>
