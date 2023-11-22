6526114579166a6487ab50dfcee787a5c8f26110
gus.hawke <gus.hawke@gmail.com>
2023-11-22 14:00:48 -0700

    run build

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+export default defineConfig(({ mode }) => {
+    return {
+        // Base configuration (both development and production)
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        // Additional configurations for production
+        ...(mode === 'production' ? {
+            // Production-specific configurations
+            // Example: Adjust the base path, enable compression, etc.
+        } : {})
+    };
 });
